3e14a7c1d39d71ad1c0e81d0579f401541423f6c
Dikens27 <kar.karovich321@gmail.com>
2024-03-08 18:40:57 +0200

    test

--- src/components/errorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-// import css from './ErrorMessage.module.css';
-
-export default function ErrorMessage() {
-  return (
-    <div>
-      <p>Sorry, something wrong, please restart page!</p>
-    </div>
-  );
-}
