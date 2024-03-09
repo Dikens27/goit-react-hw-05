012c5ea4a76715845056c334a9469bc39c2def73
Dikens27 <kar.karovich321@gmail.com>
2024-03-09 15:50:28 +0200

    test2

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,25 @@
 // import css from './App.module.css';
-// import { useState } from "react"
+import { Navigate, Route, Routes } from 'react-router-dom';
+import 
 
 export default function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/country" element={<SearchCountry />} />
+        <Route path="/country/:countryId" element={<Country />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 }
 
-// 2495b72f5ec163604337e6b2d7e7f95f
+{
+  /* // 2495b72f5ec163604337e6b2d7e7f95f
 // eyJhbGciOiJIUzI1NiJ9
 //   .eyJhdWQiOiIyNDk1YjcyZjVlYzE2MzYwNDMzN2U2YjJkN2U3Zjk1ZiIsInN1YiI6IjY1ZWIzNGRiMzg5ZGExMDE2MmQ4MmQ0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ
 //   .wOrzvtt81dJKbJMZLWNeItV9LVtFK7uogc -
 //   rXIe -
-//   JIY;
+//   JIY; */
+}
